96c65770b379def3ebe1f168226fd86f18a7ee94
weibing <weibing@winchannel.net>
2017-04-21 14:04:38 +0800

    修改及扩展表结构

--- src/main/resources/authority.sql
+++ src/main/resources/authority.sql
@@ -16,7 +16,7 @@
 */
 CREATE DATABASE `authority` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-
+-- 用户表
 CREATE TABLE `sys_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `username` varchar(32) DEFAULT NULL COMMENT '登录名',
@@ -24,10 +24,43 @@ CREATE TABLE `sys_user` (
   `password` varchar(256) DEFAULT NULL COMMENT '密码',
   `salt` varchar(32) DEFAULT NULL COMMENT '密码盐，用于密码加密',
   `mobile` varchar(20) DEFAULT NULL COMMENT '手机号',
-  `status` int(11) DEFAULT NULL COMMENT '账号状态（正常0，锁定1，删除2）',
+  `status` int(11) DEFAULT NULL COMMENT '状态（正常0，锁定1，删除2）',
   `create_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建日期',
+  `create_user` varchar(32) DEFAULT NULL COMMENT '创建人',
   `update_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改日期',
+  `update_user` varchar(32) DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `authority`.`sys_user` (`id`, `username`, `name`, `password`, `salt`, `mobile`, `status`, `create_date`, `update_date`) VALUES ('1', 'admin', 'Admin', 'ceb4f32325eda6142bd65215f4c0f371', 'admin', '13800013800', '0', '2017-04-20 02:55:34', '2017-04-20 02:55:34');
+-- 系统管理员用户
+INSERT INTO `authority`.`sys_user` (`id`, `username`, `name`, `password`, `salt`, `mobile`, `status`, `create_date`, `create_user`, `update_date`, `update_user`) VALUES ('1', 'admin', 'Admin', 'ceb4f32325eda6142bd65215f4c0f371', 'admin', '13800013800', '0', '2017-04-20 02:55:34', 'admin', '2017-04-20 02:55:34', 'admin');
+
+-- 角色表
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `code` varchar(32) DEFAULT NULL COMMENT '角色编码',
+  `name` varchar(32) DEFAULT NULL COMMENT '角色名称',
+  `remark` varchar(256) DEFAULT NULL COMMENT '备注',
+  `status` int(11) DEFAULT NULL COMMENT '状态（正常0，锁定1，删除2）',
+  `create_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建日期',
+  `create_user` varchar(32) DEFAULT NULL COMMENT '创建人',
+  `update_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改日期',
+  `update_user` varchar(32) DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--用户角色关联表
+CREATE TABLE `sys_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户PK',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色PK',
+  `status` int(11) DEFAULT NULL COMMENT '状态（正常0，锁定1，删除2）',
+  `create_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建日期',
+  `create_user` varchar(32) DEFAULT NULL COMMENT '创建人',
+  `update_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改日期',
+  `update_user` varchar(32) DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+
