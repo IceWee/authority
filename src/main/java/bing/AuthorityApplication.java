598223f722ac1632971ef2d82c47df99fe29aa90
Bing <Bing@bing-pc>
2017-04-23 10:17:30 +0800

    重新开启HTTPS

--- src/main/java/bing/AuthorityApplication.java
+++ src/main/java/bing/AuthorityApplication.java
@@ -1,10 +1,16 @@
 package bing;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +19,7 @@ import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @EnableCaching
-@ComponentScan(basePackages = { "bing" })
+@ComponentScan(basePackages = {"bing"})
 @MapperScan("bing.**.dao")
 public class AuthorityApplication {
 
@@ -36,38 +42,36 @@ public class AuthorityApplication {
 
 	// 下面的两个Bean是用来重定向HTTP请求到HTTPS，注释后即可关闭重定向
 
-	// @Bean
-	// public EmbeddedServletContainerFactory servletContainer() {
-	// TomcatEmbeddedServletContainerFactory tomcat = new
-	// TomcatEmbeddedServletContainerFactory() {
-	//
-	// @Override
-	// protected void postProcessContext(Context context) {
-	// SecurityConstraint constraint = new SecurityConstraint();
-	// constraint.setUserConstraint("CONFIDENTIAL");
-	// SecurityCollection collection = new SecurityCollection();
-	// collection.addPattern("/*");
-	// constraint.addCollection(collection);
-	// context.addConstraint(constraint);
-	// }
-	//
-	// };
-	//
-	// tomcat.addAdditionalTomcatConnectors(httpConnector());
-	// return tomcat;
-	// }
-	//
-	// @Bean
-	// public Connector httpConnector() {
-	// Connector connector = new
-	// Connector("org.apache.coyote.http11.Http11NioProtocol");
-	// connector.setScheme("http");
-	// // Connector监听的http的端口号
-	// connector.setPort(8080);
-	// connector.setSecure(false);
-	// // 监听到http的端口号后转向到的https的端口号
-	// connector.setRedirectPort(8443);
-	// return connector;
-	// }
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint constraint = new SecurityConstraint();
+				constraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				constraint.addCollection(collection);
+				context.addConstraint(constraint);
+			}
+
+		};
+
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	@Bean
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		// Connector监听的http的端口号
+		connector.setPort(8080);
+		connector.setSecure(false);
+		// 监听到http的端口号后转向到的https的端口号
+		connector.setRedirectPort(8443);
+		return connector;
+	}
 
 }
