e62031cd64286e37aadd4dbae708e66beb716371
weibing <weibing@winchannel.net>
2017-04-25 10:22:20 +0800

    抽出公共组件

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -83,3 +83,98 @@ function hideTips(id) {
 function resetForm(formId) {
 	$("#" + formId).form("reset");
 }
+
+// 初始化datagrid
+// url 必填，查询URL
+// tableId，可选，默认值：datagrid
+// toolbarId，可选，默认值：search_tools
+// pageSize，可选，默认值：10
+// searchFormId，可选，默认值：search_form
+// searchBtnId，可选，默认值：btn_search
+// resetBtnId，可选，默认值：btn_reset
+function _initDatagrid(url, tableId, toolbarId, pageSize, searchFormId, searchBtnId, resetBtnId) {
+	if (!url) {
+		console.log("查询url为空，数据将不会加载...");
+		return;
+	}
+	tableId = arguments[1] ? "#" + arguments[1] : "#datagrid";
+	toolbarId = arguments[2] ? "#" + arguments[2] : "#search_tools";
+	pageSize = arguments[3] ? arguments[3] : 10;
+	searchFormId = arguments[4] ? "#" + arguments[4] : "#search_form";
+	searchBtnId = arguments[5] ? "#" + arguments[5] : "#btn_search";
+	resetBtnId = arguments[6] ? "#" + arguments[6] : "#btn_reset";
+	$(tableId).datagrid({
+		iconCls : "icon-ok",
+		pageSize : pageSize,
+		pageList : [ pageSize, 50, 100 ],
+		singleSelect : true,
+		nowrap : true, // 设置为true，当数据长度超出列宽时将会自动截取  
+		striped : true, // 设置为true将交替显示行背景。  
+		collapsible : true, // 显示可折叠按钮  
+		toolbar : toolbarId, // 自定义工具
+		fitColumns : true, // 允许表格自动缩放，以适应父容器  
+		remoteSort : true,
+		pagination : true, // 分页 
+		rownumbers : true,
+		fitColumns : true // 自适应宽度
+	// 行数  
+	});
+	
+	// 自定义分页
+	$(tableId).datagrid("getPager").pagination({
+		onSelectPage : function() { // 翻页
+			_search(url, tableId, searchFormId);
+		}
+	});
+	
+	// 查询
+	$(searchBtnId).click(function() {
+		_search(url, tableId, searchFormId);
+	});
+	
+	// 表单重置
+	$(resetBtnId).click(function() {
+		$(searchFormId).form("reset");
+	});
+}
+
+// 列表查询
+function _search(url, tableId, searchFormId) {
+	var RESPONSE_OK = 200;
+	hideTips();
+	ajaxLoading($.i18n.prop("loading"));
+	var pager = $(tableId).datagrid("getPager");
+	var pageNo = pager.pagination("options").pageNumber;
+	if (!pageNo) {
+		pageNo = 1;
+	}
+	var pageSize = pager.pagination("options").pageSize;
+	var data = $(searchFormId).serialize();
+	data = data + "&pageNo=" + pageNo + "&pageSize=" + pageSize + "&timestamp=" + new Date().getTime();
+	$.ajax({
+		type : "GET",
+		url : url,
+		data : data,
+		success : function(json) {
+			ajaxLoaded();
+			if (json.code == RESPONSE_OK) {
+				if (json.data) {
+					var totalRows = json.data.totalRows;
+					var list = json.data.data;
+					$(tableId).datagrid("loadData", list);
+					pager.pagination({
+						total : totalRows, // 总条数  
+						pageNumber : pageNo, // 页号
+						pageSize : pageSize // 每页行数
+					});
+				}
+			} else {
+				showErrorTips(json.message);
+			}
+		},
+		error : function() {
+			ajaxLoaded();
+			showErrorTips($.i18n .prop("requestFailed"));
+		}
+	});
+}
