112d7a082ea3bbc7f592a75ea827c35650a2ec3c
weibing <weibing@winchannel.net>
2017-06-03 16:44:40 +0800

    增加打war包的支持

--- src/main/java/bing/AuthorityApplication.java
+++ src/main/java/bing/AuthorityApplication.java
@@ -1,19 +1,13 @@
 package bing;
 
-import org.apache.catalina.connector.Connector;
+//import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -21,17 +15,26 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import bing.constant.GlobalConstants;
 
-@SpringBootApplication
 // @EnableCaching
+@SpringBootApplication
 @EnableAsync // 开启异步调用
 @EnableScheduling // 允许定时任务
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = GlobalConstants.SESSION_TIMEOUT_SECONDS) // session过期时间，单位：秒
 @ComponentScan(basePackages = { GlobalConstants.COMPONENT_SCAN_PACKAGES })
 @MapperScan(GlobalConstants.MAPPER_SCAN_PACKAGES)
-public class AuthorityApplication {
+// public class AuthorityApplication extends SpringBootServletInitializer { // 打war包时打开
+public class AuthorityApplication { // 开发时打开
 
 	@Autowired
 	private MessageSource messageSource;
+
+	/**
+	 * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+	 */
+	// @Override // 打war包时打开
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AuthorityApplication.class);
+	}
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(AuthorityApplication.class).web(true).run(args);
@@ -49,54 +52,54 @@ public class AuthorityApplication {
 		return bean;
 	}
 
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-		return new EmbeddedServletContainerCustomizer() {
-			@Override
-			public void customize(ConfigurableEmbeddedServletContainer container) {
-				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-				ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/403.html");
-				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-				container.addErrorPages(error401Page, error403Page, error404Page);
-			}
-		};
-	}
+	// @Bean
+	// public EmbeddedServletContainerCustomizer containerCustomizer() {
+	// return new EmbeddedServletContainerCustomizer() {
+	// @Override
+	// public void customize(ConfigurableEmbeddedServletContainer container) {
+	// ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+	// ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/403.html");
+	// ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+	// container.addErrorPages(error401Page, error403Page, error404Page);
+	// }
+	// };
+	// }
 
 	// 下面的两个Bean是用来重定向HTTP请求到HTTPS，注释后即可关闭重定向
 
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	// @Bean
+	// public EmbeddedServletContainerFactory servletContainer() {
+	// TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	//
+	// // 打开注释开启HTTPS，同时需要解开配置文件中ssl开头的配置，并将server端口改为8443
+	// @Override
+	// protected void postProcessContext(Context context) {
+	// SecurityConstraint constraint = new SecurityConstraint();
+	// constraint.setUserConstraint("CONFIDENTIAL");
+	// SecurityCollection collection = new SecurityCollection();
+	// collection.addPattern("/*");
+	// constraint.addCollection(collection);
+	// context.addConstraint(constraint);
+	// }
+	//
+	// };
+	//
+	// tomcat.addAdditionalTomcatConnectors(httpConnector()); //
+	// // 打开注释开启HTTP转发到HTTPS
+	// tomcat.setSessionTimeout(5);
+	// return tomcat;
+	// }
 
-			// 打开注释开启HTTPS，同时需要解开配置文件中ssl开头的配置，并将server端口改为8443
-			// @Override
-			// protected void postProcessContext(Context context) {
-			// SecurityConstraint constraint = new SecurityConstraint();
-			// constraint.setUserConstraint("CONFIDENTIAL");
-			// SecurityCollection collection = new SecurityCollection();
-			// collection.addPattern("/*");
-			// constraint.addCollection(collection);
-			// context.addConstraint(constraint);
-			// }
-
-		};
-
-		// tomcat.addAdditionalTomcatConnectors(httpConnector()); //
-		// 打开注释开启HTTP转发到HTTPS
-		tomcat.setSessionTimeout(5);
-		return tomcat;
-	}
-
-	@Bean
-	public Connector httpConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		// Connector监听的http的端口号
-		connector.setPort(8080);
-		connector.setSecure(false);
-		// 监听到http的端口号后转向到的https的端口号
-		connector.setRedirectPort(8443);
-		return connector;
-	}
+	// @Bean
+	// public Connector httpConnector() {
+	// Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	// connector.setScheme("http");
+	// // Connector监听的http的端口号
+	// connector.setPort(8080);
+	// connector.setSecure(false);
+	// // 监听到http的端口号后转向到的https的端口号
+	// connector.setRedirectPort(8443);
+	// return connector;
+	// }
 
 }
