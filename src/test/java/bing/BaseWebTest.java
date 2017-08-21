9725cda011dc7d7e27f0e392e67cb9b3997a2dd6
weibing <weibing@xiaoshouyi.com>
2017-08-21 15:00:51 +0800

    修改日志工具，由log4j2换回logback

--- src/test/java/bing/BaseWebTest.java
+++ src/test/java/bing/BaseWebTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,5 +16,8 @@ public class BaseWebTest {
 
 	@Autowired
 	protected MockMvc mockMvc;
+
+	@Autowired
+	protected TestRestTemplate restTemplate;
 
 }
