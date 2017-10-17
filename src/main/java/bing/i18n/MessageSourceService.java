09659c3d688f0b16485291f4aaa0dd3809ab8ad6
魏冰 <weibing@xiaoshouyi.com>
2017-10-17 15:44:52 +0800

    国际化服务类优化

--- src/main/java/bing/i18n/MessageSourceService.java
+++ src/main/java/bing/i18n/MessageSourceService.java
@@ -1,41 +1,32 @@
 package bing.i18n;
 
-import java.util.Locale;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
 @Service
 public class MessageSourceService {
 
-	@Resource
-	private MessageSource messageSource;
+    @Resource
+    private MessageSource messageSource;
 
-	public String getMessage(String code) {
-		return getMessage(code, null);
-	}
+    public String getMessage(String code, Object... args) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return getMessageWithLocale(locale, code, args);
+    }
 
-	public String getMessage(String code, Object[] args) {
-		Locale locale = LocaleContextHolder.getLocale();
-		return getMessageWithLocale(code, args, locale);
-	}
+    public String getMessage(HttpServletRequest request, String code, Object... args) {
+        Locale locale = RequestContextUtils.getLocale(request);
+        return getMessageWithLocale(locale, code, args);
+    }
 
-	public String getMessage(String code, Object[] args, HttpServletRequest request) {
-		Locale locale = RequestContextUtils.getLocale(request);
-		return getMessageWithLocale(code, args, locale);
-	}
-
-	public String getMessageWithLocale(String code, Locale locale) {
-		return getMessageWithLocale(code, null, locale);
-	}
-
-	public String getMessageWithLocale(String code, Object[] args, Locale locale) {
-		return messageSource.getMessage(code, args, locale);
-	}
+    public String getMessageWithLocale(Locale locale, String code, Object... args) {
+        return messageSource.getMessage(code, args, locale);
+    }
 
 }
